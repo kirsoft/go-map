6661360d9fd0ad81306dbbf76d437e136efdd18b
Alexander Litvinov <2720927+you-think-you-are-special@users.noreply.github.com>
2019-05-27 17:46:44 +0300

    Add rust hashbrown and rustc-hash benchmarks

--- rust/hashmap/src/bench.rs
+++ rust/hashmap/src/bench.rs
@@ -32,3 +32,61 @@ fn read_write_bench(b: &mut Bencher) {
         let _r = super::read(&mut m);
     });
 }
+
+#[bench]
+fn fx_write_bench(b: &mut Bencher) {
+    let mut m = super::fx_crete_hashmap();
+
+    b.iter(|| {
+        let _i = super::fx_write(&mut m);
+    });
+}
+
+#[bench]
+fn fx_read_bench(b: &mut Bencher) {
+    let mut m = super::fx_crete_hashmap();
+    super::fx_write(&mut m);
+
+    b.iter(|| {
+        let _r = super::fx_read(&mut m);
+    });
+}
+
+#[bench]
+fn fx_read_write_bench(b: &mut Bencher) {
+    let mut m = super::fx_crete_hashmap();
+
+    b.iter(|| {
+        super::fx_write(&mut m);
+        let _r = super::fx_read(&mut m);
+    });
+}
+
+#[bench]
+fn hashbrown_write_bench(b: &mut Bencher) {
+    let mut m = super::fx_crete_hashmap();
+
+    b.iter(|| {
+        let _i = super::fx_write(&mut m);
+    });
+}
+
+#[bench]
+fn hashbrown_read_bench(b: &mut Bencher) {
+    let mut m = super::fx_crete_hashmap();
+    super::fx_write(&mut m);
+
+    b.iter(|| {
+        let _r = super::fx_read(&mut m);
+    });
+}
+
+#[bench]
+fn hashbrown_read_write_bench(b: &mut Bencher) {
+    let mut m = super::fx_crete_hashmap();
+
+    b.iter(|| {
+        super::fx_write(&mut m);
+        let _r = super::fx_read(&mut m);
+    });
+}
--- rust/hashmap/src/lib.rs
+++ rust/hashmap/src/lib.rs
@@ -1,5 +1,8 @@
 #![feature(test)]
 use std::collections::HashMap;
+use rustc_hash::FxHashMap;
+use hashbrown::HashMap as HbHashMap;
+
 
 pub mod bench;
 pub mod test;
@@ -23,6 +26,56 @@ fn write(m: &mut HashMap<String, String>) -> &mut HashMap<String, String> {
 }
 
 fn read(m: &mut HashMap<String, String>) -> &mut HashMap<String, String> {
+    for i in 0..N {
+        let mut k = i.to_string();
+        k.push_str("-string");
+        m.get(&k);
+    }
+
+    m
+}
+
+fn fx_crete_hashmap() -> FxHashMap<String, String> {
+    FxHashMap::default()
+}
+
+
+fn fx_write(m: &mut FxHashMap<String, String>) -> &mut FxHashMap<String, String> {
+    for i in 0..N {
+        let mut k = i.to_string();
+        k.push_str("-string");
+        m.insert(k.clone(), k);
+    }
+
+    m
+}
+
+fn fx_read(m: &mut FxHashMap<String, String>) -> &mut FxHashMap<String, String> {
+    for i in 0..N {
+        let mut k = i.to_string();
+        k.push_str("-string");
+        m.get(&k);
+    }
+
+    m
+}
+
+fn hashbrown_crete_hashmap() -> HbHashMap<String, String> {
+    HbHashMap::default()
+}
+
+
+fn hashbrown_write(m: &mut HbHashMap<String, String>) -> &mut HbHashMap<String, String> {
+    for i in 0..N {
+        let mut k = i.to_string();
+        k.push_str("-string");
+        m.insert(k.clone(), k);
+    }
+
+    m
+}
+
+fn hashbrown_read(m: &mut HbHashMap<String, String>) -> &mut HbHashMap<String, String> {
     for i in 0..N {
         let mut k = i.to_string();
         k.push_str("-string");
